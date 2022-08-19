02bc358a31270f2c1254d9bd732af61834b77b8b
SebastianRosenquist <seb_rosenquist@hotmail.com>
2022-08-19 18:32:40 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FlatListComponent from "./components/FlatListComponent";
+import ArrayListComponent from "./components/ArrayListComponent";
+import FetchListComponent from "./components/FetchListComponent";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <FlatListComponent/>
+        <ArrayListComponent/>
+        <FetchListComponent/>
+      </View>
   );
 }
 
@@ -14,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
